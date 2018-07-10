24e75823dacf67cc05c02d5d24400eb06390d15c
huo <wtx05@126.com>
2018-07-10 23:32:39 +0800

    modify analyse script

--- rtp_parser/bin/analyse.py
+++ rtp_parser/bin/analyse.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-import numpy as np
+import sys
 import matplotlib.pyplot as plt
 
-def get_timecost_array(filename):
+def get_timecost_list(filename):
     timecost_list = []
     last_seq = -1;
     
@@ -17,37 +17,31 @@ def get_timecost_array(filename):
                 last_seq = int(seq_str)
             else:
                 seq = int(seq_str)
-                if (seq != last_seq + 1) and (last_seq != 65535):
+                if seq != ((last_seq + 1) % 65535):
                     print("lost rtp:%s"  %(line))
                 last_seq = seq
                 
         if line.find("Frm_Interval") < 0:
             continue;
         
-        timecost_str = line[-12:-4]
-        if int(timecost_str) >= 200000:
-            print(line)
-        timecost_list.append(timecost_str)
+        timecost = int(line[-12:-4])
+        if timecost >= 100000:     #cost too many time, more than 100ms
+            print("Cost too many time:%s"  %(line))
+        timecost_list.append(timecost)
 
     fo.close()
 
-    tmp = np.array(timecost_list)
-    timecost_array = tmp.astype(np.float)
-    return timecost_array
+    return timecost_list
 
 
-speed_array1 = get_timecost_array("src[192.168.42.1[554]]--dst[192.168.42.102[49108]].txt")
-#speed_array2 = get_speed_array("iperf2.txt")
-#speed_array3 = get_speed_array("iperf3.txt")
+timecost_list = get_timecost_list(str(sys.argv[1]))
 
-#x = range(max(len(speed_array1), len(speed_array2), len(speed_array3)))
-x = range(len(speed_array1))
 
-#print(x)
+x = range(len(timecost_list))
 
-plt.plot(x, speed_array1, c='b')
-#plt.plot(x, speed_array2, c='g')
-#plt.plot(x, speed_array3, c='r')
+
+plt.plot(x, timecost_list, c='b')
+
 
 plt.xlabel("Frames") #X轴标签  
 plt.ylabel("Frame interval(us)")  #Y轴标签  
